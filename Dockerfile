992a41c09077a3700dbc9bb3426b129b56982268
Ubuntu <ubuntu@ip-172-31-34-147.eu-west-1.compute.internal>
2026-04-18 14:26:35 +0000

    instance changes

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,22 @@
 # 1. Use an official, lightweight Python runtime
-FROM python:3.10-slim-bullseye
+FROM python:3.12-slim-bookworm
 
 # 2. Set environment variables
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
+
+RUN apt-get update && apt-get install -y \
+    gcc \
+    default-libmysqlclient-dev \
+    pkg-config \
+    && rm -rf /var/lib/apt/lists/*
 
 # 3. Set the base working directory inside the container
 WORKDIR /app
 
 # 4. Copy the requirements file and install dependencies
 COPY requirements.txt /app/
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install -r requirements.txt
 
 # 5. Copy the entire AINN_PROJECT folder into the container
 COPY . /app/
--- carbon_project/carbon_project/settings.py
+++ carbon_project/carbon_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-bon=iq$u(b=+x^ufmtj*fp97nae#18l9g-48#lbh-k@^(hddyl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.51.150.204', 'localhost', '127.0.0.1', '0.0.0.0','108.129.95.37']
 
 
 # Application definition
@@ -76,10 +77,10 @@ WSGI_APPLICATION = 'carbon_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecologistics_db',        
+        'NAME': 'ecologistics',        
         'USER': 'root',                   
         'PASSWORD': 'root',  # <-- Put your actual MySQL password here!
-        'HOST': 'localhost',
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
         'PORT': '3306',
     }
 }
